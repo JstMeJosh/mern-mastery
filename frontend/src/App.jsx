e4e5034a15cfb002e2eed91e3d9da21ac36648a2
JOSHUA <joshuannorom2005@gmail>
2026-04-30 11:53:53 +0100

    add root redirect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/verify-otp" element={<VerifyOTP />} />
