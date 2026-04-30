7fd4ca691ea89d973997149c2120e643ab7847cd
JOSHUA <joshuannorom2005@gmail>
2026-04-30 13:30:12 +0100

    switch to brevo smtp

--- BACKEND/utils/sendEmail.js
+++ BACKEND/utils/sendEmail.js
@@ -2,7 +2,8 @@ import nodemailer from "nodemailer";
 
 const sendEmail = async (to, subject, html) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp-relay.brevo.com",
+    port: 587,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
