6fddc7daa11ec2e4c951f296bd5c0d7c5028fcdb
JOSHUA <joshuannorom2005@gmail>
2026-04-30 11:56:16 +0100

    add root redirect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes,Navigate } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
